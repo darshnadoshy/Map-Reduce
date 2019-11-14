95c9c72646ae70a083ef3fad9ef6c80dc11fe29c
DARSHNA Doshy <darshna@royal-05.cs.wisc.edu>
2019-11-13 18:13:52 -0600

    Should not have stuff in mapreduce.h and other changes

--- mapreduce.c
+++ mapreduce.c
@@ -1,33 +1,40 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <semaphore.h>
 #include "mapreduce.h"
 
-void Map(char *file_name) {
-    FILE *fp = fopen(file_name, "r");
-    assert(fp != NULL);
+// Declaring data structure
+typedef struct MR {
+  char *key;
+  char *value;
+  int partition_num;
+};
+// What should be the SIZE ?? Dynamically allocate the structure ?? 
 
-    char *line = NULL;
-    size_t size = 0;
-    while (getline(&line, &size, fp) != -1) {
-        char *token, *dummy = line;
-        while ((token = strsep(&dummy, " \t\n\r")) != NULL) {
-            MR_Emit(token, "1");
-        }
-    }
-    free(line);
-    fclose(fp);
+void MR_Emit(char *key, char *value) {
+    // Take key and value from different mappers and store them in a partition
+    // such that later reducers can access them, given constraints. 
 }
 
-void Reduce(char *key, Getter get_next, int partition_number) {
-    int count = 0;
-    char *value;
-    while ((value = get_next(key, partition_number)) != NULL)
-        count++;
-    printf("%s %d\n", key, count);
+// Got it from the specs
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *key++) != '\0')
+        hash = hash * 33 + c;
+    return hash % num_partitions;
 }
 
-int main(int argc, char *argv[]) {
-    MR_Run(argc, argv, Map, 10, Reduce, 10, MR_DefaultHashPartition, 10);
+unsigned long MR_SortedPartition(char *key, int num_partitions) {
+    // ensures  that keys are in a sorted order across the partitions 
+    // (i.e., keys are not hashed into random partitions as in the default partition function)
 }
+
+void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce, 
+            int num_reducers, Partitioner partition, int num_partitions) {
+    // Use Mapper to map filenames to Map() ??
+}
+
