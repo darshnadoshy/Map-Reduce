d81d0d7ea7a150bfb5dfc2c4bcdca12122ae638f
DARSHNA Doshy <darshna@royal-04.cs.wisc.edu>
2019-11-16 14:59:28 -0600

    mapper_exe

--- mapreduce.c
+++ mapreduce.c
@@ -13,9 +13,25 @@ typedef struct MR {
   int partition_num;
 }*table;
 
+typedef struct fileName {
+    char *argv[];
+    int argc;
+}*fname1;
+
+Map maps;
+Reducer reduces;
+Partitioner partitions;
+
 void MR_Emit(char *key, char *value) {
     // Take key and value from different mappers and store them in a partition
     // such that later reducers can access them, given constraints. 
+
+    table[partitions] = malloc(sizeof(MR));
+    if (table[partitions] == NULL)
+    {
+        printf("Memory Allocation Failed!\n");
+        exit(1);
+    }
 }
 
 // Got it from the specs
@@ -30,6 +46,8 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
 unsigned long MR_SortedPartition(char *key, int num_partitions) {
     // ensures  that keys are in a sorted order across the partitions 
     // (i.e., keys are not hashed into random partitions as in the default partition function)
+
+    // single call to qsort for each partition
 }
 
 
@@ -37,6 +55,10 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
             int num_reducers, Partitioner partition, int num_partitions) {
     // Use Mapper to map filenames to Map() ??
     int i,j;
+
+    maps = map;
+    reduces = reduce;
+    partitions = partition;
     // struct MR params[num_mappers];
     pthread_t p[num_mappers];
     pthread_t p[num_reducers];
@@ -52,8 +74,13 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
     // Need to do some sort of scheduling to map the files to the mappers
     // and maybe pass those as parameters to mappers_exe
 
+    for(i = 0; i < argc - 1; i++) {
+        fname1[i]->argc = argc;
+        fname1[i]->argv = argv;
+    }
+
     for(i = 0; i < num_mappers; i++) {
-        pthread_create(&p[i], NULL, mappers_exe, (void *)&params[i]);
+        pthread_create(&p[i], NULL, mappers_exe, (void *)&fname1[i]);
     }
 
     for(i = 0; i < num_mappers; i++) {
@@ -62,10 +89,15 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
 }
 
 void *mappers_exe(void *arg) { 
-    // struct MR *params = (struct MR *)arg;
-    // for(int i = 0;i < argc - 1; i++) {
-    //     map(argv[i]);
-    // }
+    // Case 1: Calling map once for each file
+    struct fileName *fname2 = (struct MR *)arg;
+    for(int i = 0;i < fname2->argc - 1; i++) {
+        map(fname2->argv[i]);
+    }
     return NULL;
 }
 
+void sort() {
+    // Sort the table in ascending order of key/value pairs
+
+}
